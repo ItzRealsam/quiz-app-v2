6ae1fed50c42171d2ced48f76bc00671ee0e070b
Samuel Nwaokocha <81534141+ItzRealsam@users.noreply.github.com>
2026-06-11 15:48:54 +0100

    improve responsiveness

--- js/ui/renderAppShell.js
+++ js/ui/renderAppShell.js
@@ -1,33 +1,41 @@
 export function renderAppShell() {
+
   return `
-    <main class="quiz">
 
-      <section class="quiz__shell container">
+    <div class="quiz__shell">
 
-        <header class="quiz__header">
+      <main class="quiz__card">
+
+        <div
+          class="quiz__card-header"
+        >
 
           <div class="quiz__brand">
 
-            <span class="quiz__brand-logo">
+            <span
+              class="
+                quiz__brand-logo
+              "
+            >
               <img src="./assets/images/realsam_logo_light.png" alt="The Realsam Logo" class="quiz__brand-logo">
             </span>
 
-            <span class="quiz__brand-text">
-              Quiz App v2
+            <span>
+              QuizMaster
             </span>
 
           </div>
 
-        </header>
-
-        <div
-          class="quiz__screen"
-          id="quiz-screen"
-        >
         </div>
 
-      </section>
+        <div 
+          class="quiz__screen"
+          id="quiz-screen">
+        </div>
 
-    </main>
+      </main>
+
+    </div>
+
   `;
 }
